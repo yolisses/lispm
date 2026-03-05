5e114bfc3c038c0e46443db3c4fa382ebb8e6431
yolisses <iamurise@gmail.com>
2026-03-05 18:07:15 -0300

    Start coding

---
+++ Software/firmware/firmware.v
@@ -0,0 +1,9 @@
+// The firmware work is to control the low level components, like ADC and DAC's,
+// and run the scan loop (due the required high speed). It works somewhat
+// similar to an external API, exposing a few functions like start scan and set
+// parameter.
+
+module top
+
+    
+endmodule
