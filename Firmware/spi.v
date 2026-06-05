c7f96df707d7883a7c38b4c04ad9a0e862e0c545
Yolisses <iamurise@gmail.com>
2026-06-05 09:37:01 -0300

    Replace "LiSTM" by "LiSPM" accross files

--- Firmware/spi.v
+++ Firmware/spi.v
@@ -5,7 +5,7 @@
 // - MOSI: Master Out Slave In. Used to send data from the master to the slave.
 // - MISO: Master In Slave Out. Used to send data from the slave to the master.
 // - SS: Slave select. It uses high or low to indicate to the slave it if it
-//   should be active. In the context of the LiSTM, all the SPI devices (DAC's
+//   should be active. In the context of the LiSPM, all the SPI devices (DAC's
 //   and ADC) can be always on, so this can be hard wired.
 
 // For simplicity, all the SPI devices connect to the same clock, produced
