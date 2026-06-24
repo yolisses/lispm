ae36ed0db4ec9c1fde4e1a16148d9b2758892052
Yolisses <iamurise@gmail.com>
2026-06-24 18:12:19 -0300

    Create AFM oscillator simulation

---
+++ Electronics/AFM Oscillator/74HCU04/tssop.s
@@ -0,0 +1,163 @@
+* TSSOP PACKAGE LIBRARY
+* NETLIST OUTPUT FOR HSPICE.
+* GENERATED ON MARCH 30, 2011
+*
+*     (C) Copyright 2011 NXP Semiconductors
+*     All rights reserved
+*     NXP Semiconductors warrants and represents that the Spice model
+*     identified herein is owned by NXP Semiconductors (NXP). The
+*     model provided is not warranted as fully representing all of the
+*     operating characteristics and specifications of the semiconductor
+*     products to which the model relates.  Moreover, unless there is a
+*     signed, written agreement to the contrary, this Spice model is
+*     furnished on an "as is" basis without support or warranty of any
+*     kind, either expressed or implied, regarding the use thereof, and
+*     NXP specifically disclaims all implied warranties of
+*     merchantability and fitness of the model for any purpose.
+*
+*     NXP does not assume any liability arising out of the application
+*     or use of this Spice model including infringement of patents and
+*     copyrights, nor does NXP convey any license under its patents and
+*     copyrights or the rights of others.
+*
+*     NXP provides Spice models as a service to our customers. You and
+*     your company shall not distribute, convey, sell, or give these models
+*     to anyone else without receiving prior written permission from
+*     NXP.
+*
+*     NXP reserves the right to make changes without notice to these
+*     Spice models to improve reliability, function, or design.
+*
+*     Please be aware that your receipt and subsequent use of the SPICE
+*     information provided shall serve as acceptance of the terms and
+*     conditions of this agreement.  If you do not accept these terms,
+*     you should return or destroy the SPICE models and any other
+*     accompanying information immediately.
+*
+.SUBCKT pk14 
++    1   2   3   4   5   6   7   8   9  10
++   11  12  13  14
++   15  16  17  18  19  20  21  22  23  24
++   25  26  27  28
+* Pin Resistance
+r1 1 1001 9.257E-02
+r2 2 1002 9.070E-02
+r3 3 1003 8.892E-02
+r4 4 1004 8.839E-02
+r5 5 1005 8.891E-02
+r6 6 1006 9.070E-02
+r7 7 1007 9.258E-02
+r8 8 1008 9.256E-02
+r9 9 1009 9.068E-02
+r10 10 1010 8.890E-02
+r11 11 1011 8.837E-02
+r12 12 1012 8.890E-02
+r13 13 1013 9.068E-02
+r14 14 1014 9.256E-02
+
+* Pin Inductance
+l1 1001 15 2.948E-09
+l2 1002 16 2.549E-09
+l3 1003 17 2.345E-09
+l4 1004 18 2.310E-09
+l5 1005 19 2.344E-09
+l6 1006 20 2.547E-09
+l7 1007 21 2.944E-09
+l8 1008 22 2.942E-09
+l9 1009 23 2.546E-09
+l10 1010 24 2.342E-09
+l11 1011 25 2.308E-09
+l12 1012 26 2.345E-09
+l13 1013 27 2.550E-09
+l14 1014 28 2.947E-09
+.ENDS pk14
+.SUBCKT pk16 
++    1   2   3   4   5   6   7   8   9  10
++   11  12  13  14  15  16
++   17  18  19  20  21  22  23  24  25  26
++   27  28  29  30  31  32
+* Pin Resistance
+r1 1 1001 9.213E-02
+r2 2 1002 9.808E-02
+r3 3 1003 9.628E-02
+r4 4 1004 9.194E-02
+r5 5 1005 9.196E-02
+r6 6 1006 9.626E-02
+r7 7 1007 9.806E-02
+r8 8 1008 9.209E-02
+r9 9 1009 9.214E-02
+r10 10 1010 9.806E-02
+r11 11 1011 9.627E-02
+r12 12 1012 9.196E-02
+r13 13 1013 9.196E-02
+r14 14 1014 9.628E-02
+r15 15 1015 9.807E-02
+r16 16 1016 9.208E-02
+
+* Pin Inductance
+l1 1001 17 2.955E-09
+l2 1002 18 2.647E-09
+l3 1003 19 2.414E-09
+l4 1004 20 2.337E-09
+l5 1005 21 2.337E-09
+l6 1006 22 2.413E-09
+l7 1007 23 2.648E-09
+l8 1008 24 2.949E-09
+l9 1009 25 2.956E-09
+l10 1010 26 2.648E-09
+l11 1011 27 2.417E-09
+l12 1012 28 2.340E-09
+l13 1013 29 2.336E-09
+l14 1014 30 2.412E-09
+l15 1015 31 2.647E-09
+l16 1016 32 2.947E-09
+.ENDS pk16
+.SUBCKT pk20 
++    1   2   3   4   5   6   7   8   9  10
++   11  12  13  14  15  16  17  18  19  20
++   21  22  23  24  25  26  27  28  29  30
++   31  32  33  34  35  36  37  38  39  40
+* Pin Resistance
+r1 1 1001 9.132E-02
+r2 2 1002 8.924E-02
+r3 3 1003 8.600E-02
+r4 4 1004 8.597E-02
+r5 5 1005 8.576E-02
+r6 6 1006 8.574E-02
+r7 7 1007 8.595E-02
+r8 8 1008 8.594E-02
+r9 9 1009 8.925E-02
+r10 10 1010 9.130E-02
+r11 11 1011 9.134E-02
+r12 12 1012 8.925E-02
+r13 13 1013 8.600E-02
+r14 14 1014 8.597E-02
+r15 15 1015 8.576E-02
+r16 16 1016 8.574E-02
+r17 17 1017 8.596E-02
+r18 18 1018 8.595E-02
+r19 19 1019 8.925E-02
+r20 20 1020 9.134E-02
+
+* Pin Inductance
+l1 1001 21 2.811E-09
+l2 1002 22 2.335E-09
+l3 1003 23 1.921E-09
+l4 1004 24 1.922E-09
+l5 1005 25 1.914E-09
+l6 1006 26 1.913E-09
+l7 1007 27 1.921E-09
+l8 1008 28 1.921E-09
+l9 1009 29 2.334E-09
+l10 1010 30 2.809E-09
+l11 1011 31 2.812E-09
+l12 1012 32 2.332E-09
+l13 1013 33 1.920E-09
+l14 1014 34 1.921E-09
+l15 1015 35 1.914E-09
+l16 1016 36 1.913E-09
+l17 1017 37 1.921E-09
+l18 1018 38 1.921E-09
+l19 1019 39 2.331E-09
+l20 1020 40 2.812E-09
+.ENDS pk20
