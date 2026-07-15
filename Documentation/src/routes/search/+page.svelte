c47f86a571880d7122028c4902b6409135da15ff
Yolisses <iamurise@gmail.com>
2026-07-15 10:45:14 -0300

    Separate SearchPage

--- Documentation/src/routes/search/+page.svelte
+++ Documentation/src/routes/search/+page.svelte
@@ -1,42 +1,5 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
-
-	let searchElement: HTMLElement | null = $state(null);
-
-	onMount(() => {
-		const loadPagefindUi = () => {
-			const PagefindUI = window.PagefindUI;
-
-			if (!PagefindUI || !searchElement) {
-				return;
-			}
-
-			new PagefindUI({
-				element: searchElement,
-				showImages: false
-			});
-		};
-
-		if (typeof window === 'undefined') {
-			return;
-		}
-
-		if (window.PagefindUI) {
-			loadPagefindUi();
-			return;
-		}
-
-		const script = document.createElement('script');
-		script.src = '/pagefind/pagefind-ui.js';
-		script.onload = loadPagefindUi;
-		document.head.appendChild(script);
-	});
+<script>
+	import SearchPage from '$lib/SearchPage.svelte';
 </script>
 
-<svelte:head>
-	<link rel="stylesheet" href="/pagefind/pagefind-ui.css" />
-</svelte:head>
-
-<div class="mx-auto max-w-3xl p-8">
-	<div bind:this={searchElement} class="mt-6"></div>
-</div>
+<SearchPage />
