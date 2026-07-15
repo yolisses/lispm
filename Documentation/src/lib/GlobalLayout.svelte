fe968433a4f226e083c20fdc3e6f1911010df837
Yolisses <iamurise@gmail.com>
2026-07-15 09:59:51 -0300

    Create ThemeButton and LogoButton

--- Documentation/src/lib/GlobalLayout.svelte
+++ Documentation/src/lib/GlobalLayout.svelte
@@ -1,38 +1,6 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-
 	import SidePanel from './SidePanel.svelte';
 	const { children } = $props();
-	let searchElement: HTMLElement | null = $state(null);
-
-	onMount(() => {
-		if (typeof window === 'undefined') {
-			return;
-		}
-
-		const loadPagefindUi = () => {
-			const PagefindUI = window.PagefindUI;
-
-			if (!PagefindUI || !searchElement) {
-				return;
-			}
-
-			new PagefindUI({
-				element: searchElement,
-				showImages: false
-			});
-		};
-
-		if (window.PagefindUI) {
-			loadPagefindUi();
-			return;
-		}
-
-		const script = document.createElement('script');
-		script.src = '/pagefind/pagefind-ui.js';
-		script.onload = loadPagefindUi;
-		document.head.appendChild(script);
-	});
 </script>
 
 <svelte:head>
@@ -41,7 +9,9 @@
 
 <div class="flex">
 	<SidePanel />
-	<main class="p-2 flex justify-center">
-		{@render children()}
-	</main>
+	<div class="flex justify-center flex-1 p-2">
+		<main class="justify-center w-full max-w-2xl">
+			{@render children()}
+		</main>
+	</div>
 </div>
--- Documentation/src/lib/SidePanel.svelte
+++ Documentation/src/lib/SidePanel.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
-	import { resolve } from '$app/paths';
-	import logo from '$lib/assets/logo text white.svg';
 	import GitHubButton from './GitHubButton.svelte';
+	import LogoButton from './LogoButton.svelte';
 	import Navbar from './Navbar.svelte';
 	import SearchButton from './SearchButton.svelte';
+	import ThemeButton from './ThemeButton.svelte';
 </script>
 
-<aside class="bg-zinc-900 sticky top-0 max-h-dvh overflow-hidden p-2 flex flex-col">
+<aside
+	class="dark:bg-zinc-900 bg-zinc-200 sticky top-0 max-h-dvh overflow-hidden p-2 flex flex-col"
+>
 	<div class="flex py-2 justify-between items-center border-b dark:border-white/10 border-black/10">
-		<a href={resolve('/')} class="basic-button">
-			<img src={logo} width="100" alt="LiSPM documentation home" />
-		</a>
-		<SearchButton />
+		<LogoButton />
+		<div class="flex gap-1">
+			<ThemeButton />
+			<SearchButton />
+		</div>
 	</div>
 	<Navbar />
 	<div class="flex justify-between items-center border-t dark:border-white/10 border-black/10">
