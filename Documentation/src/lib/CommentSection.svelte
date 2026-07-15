61358cdc5c7122eed2a86787555e9cbc893b6a7d
Yolisses <iamurise@gmail.com>
2026-07-15 12:11:44 -0300

    Fix giscus reloading

---
+++ Documentation/src/lib/CommentSection.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Giscus from '@giscus/svelte';
+	import { themeIsDark } from './themeIsDark';
+</script>
+
+<Giscus
+	id="comments"
+	repo="yolisses/lispm"
+	repoId="R_kgDORffP2w"
+	category="Announcements"
+	categoryId="DIC_kwDORffP284DAkxD"
+	mapping="pathname"
+	term="Welcome to giscus!"
+	reactionsEnabled="0"
+	emitMetadata="0"
+	inputPosition="top"
+	theme={$themeIsDark ? 'noborder_gray' : 'noborder_light'}
+	lang="en"
+	loading="lazy"
+></Giscus>
