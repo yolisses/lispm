aebc8ab9425d30035bc68250faae2d7999e373dd
Yolisses <iamurise@gmail.com>
2026-07-15 10:13:46 -0300

    Implement in memory theme switch

--- Documentation/src/lib/SidePanel.svelte
+++ Documentation/src/lib/SidePanel.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <aside
-	class="dark:bg-zinc-900 bg-zinc-200 sticky top-0 max-h-dvh overflow-hidden p-2 flex flex-col"
+	class="dark:bg-zinc-950 bg-zinc-200 sticky top-0 max-h-dvh overflow-hidden p-2 flex flex-col"
 >
 	<div class="flex py-2 justify-between items-center border-b dark:border-white/10 border-black/10">
 		<LogoButton />
