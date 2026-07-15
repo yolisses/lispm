ca211c46ef042b3d3fa53a1d47671c0ab1197259
Yolisses <iamurise@gmail.com>
2026-07-15 09:36:00 -0300

    Format

--- Documentation/src/lib/DocNavSection.svelte
+++ Documentation/src/lib/DocNavSection.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	import type { DocTreeNode } from './tree/content';
-
+	import type { DocTreeNode } from './tree/DocTreeNode';
 	const { node }: { node: DocTreeNode } = $props();
 </script>
 
 <div class="flex flex-col gap-1">
 	{#if node.type === 'folder'}
 		{#if node.indexDoc}
-			<a href={`/${node.indexDoc.routeSlug}`} class="basic-button">{node.name}</a>
+			<a href={'/' + node.indexDoc.routeSlug} class="basic-button">{node.name}</a>
 		{:else}
 			<div class="font-semibold">{node.name}</div>
 		{/if}
@@ -23,11 +22,11 @@
 		{#if node.files.length}
 			<div class="ml-3 flex flex-col gap-1">
 				{#each node.files as doc}
-					<a href={`/${doc.routeSlug}`} class="basic-button">{doc.title}</a>
+					<a href={'/' + doc.routeSlug} class="basic-button">{doc.title}</a>
 				{/each}
 			</div>
 		{/if}
 	{:else}
-		<a href={`/${node.doc.routeSlug}`} class="basic-button">{node.doc.title}</a>
+		<a href={'/' + node.doc.routeSlug} class="basic-button">{node.doc.title}</a>
 	{/if}
 </div>
