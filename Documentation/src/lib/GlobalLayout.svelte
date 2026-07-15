ac296efdbcdd67b3f31fc97a01c23892246c9470
Yolisses <iamurise@gmail.com>
2026-07-15 00:15:20 -0300

    Create basic navbar

---
+++ Documentation/src/lib/GlobalLayout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Navbar from './Navbar.svelte';
+
+	const { children } = $props();
+</script>
+
+<div class="flex flex-1 min-h-svh">
+	<aside class="bg-zinc-900">
+		<div class="sticky top-0 p-2">
+			Contents
+			<Navbar />
+		</div>
+	</aside>
+	<main class="p-2">
+		{@render children()}
+	</main>
+</div>
--- Documentation/src/routes/+layout.svelte
+++ Documentation/src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
-	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import GlobalLayout from '$lib/GlobalLayout.svelte';
+	import './layout.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<GlobalLayout>
+	<div class="prose dark:prose-invert">
+		{@render children()}
+	</div>
+</GlobalLayout>
