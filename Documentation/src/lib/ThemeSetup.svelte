4e1505c13331fe31db5c2089826b97a6a8a76417
Yolisses <iamurise@gmail.com>
2026-07-15 10:17:23 -0300

    Separate ThemeSetup

---
+++ Documentation/src/lib/ThemeSetup.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { themeIsDark } from './themeIsDark';
+
+	themeIsDark.subscribe((value) => {
+		if (typeof document != 'undefined') {
+			document.documentElement.classList.toggle('dark', value);
+		}
+	});
+</script>
