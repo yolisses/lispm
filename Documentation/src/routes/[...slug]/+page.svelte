84835851c260fead071780bd909b2b2a7a29947a
Yolisses <iamurise@gmail.com>
2026-07-15 12:02:54 -0300

    Add comments with giscus

--- Documentation/src/routes/[...slug]/+page.svelte
+++ Documentation/src/routes/[...slug]/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { themeIsDark } from '$lib/themeIsDark';
 	import { getDoc } from '$lib/tree/getDoc';
+	import Giscus from '@giscus/svelte';
 
 	const slug = $derived(
 		Array.isArray($page.params.slug) ? $page.params.slug.join('/') : ($page.params.slug ?? '')
@@ -11,6 +13,21 @@
 {#if doc}
 	{@const DocComponent = doc.default as any}
 	<DocComponent />
+	<Giscus
+		id="comments"
+		repo="yolisses/lispm"
+		repoId="R_kgDORffP2w"
+		category="Announcements"
+		categoryId="DIC_kwDORffP284DAkxD"
+		mapping="pathname"
+		term="Welcome to giscus!"
+		reactionsEnabled="0"
+		emitMetadata="0"
+		inputPosition="top"
+		theme={$themeIsDark ? 'noborder_gray' : 'noborder_light'}
+		lang="en"
+		loading="lazy"
+	></Giscus>
 {:else}
 	<p>Document not found.</p>
 {/if}
