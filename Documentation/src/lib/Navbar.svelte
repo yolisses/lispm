22bd26d9af407e329e7335f1d40c5fc5112cb7a6
Yolisses <iamurise@gmail.com>
2026-07-15 00:20:09 -0300

    Add basic navigation to content

--- Documentation/src/lib/Navbar.svelte
+++ Documentation/src/lib/Navbar.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
 	import { docs } from './content';
-	console.log(docs);
 </script>
 
 <nav class="flex flex-col">
 	{#each docs as doc (doc.slug)}
-		<a href="/doc.link" class="basic-button">{doc.slug}</a>
+		<a href={`/${encodeURIComponent(doc.slug)}`} class="basic-button">{doc.slug}</a>
 	{/each}
 </nav>
