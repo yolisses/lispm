1570e11f6d16b0f65c2d521465e01fc780a2541b
Yolisses <iamurise@gmail.com>
2026-07-15 00:19:55 -0300

    Add logo on top of navbar

--- Documentation/src/lib/GlobalLayout.svelte
+++ Documentation/src/lib/GlobalLayout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import Navbar from './Navbar.svelte';
 
+	import logo from '$lib/assets/logo text white.svg';
 	const { children } = $props();
 </script>
 
 <div class="flex flex-1 min-h-svh">
 	<aside class="bg-zinc-900">
+		<a href="/" class="basic-button">
+			<img src={logo} width="100" />
+		</a>
 		<div class="sticky top-0 p-2">
 			Contents
 			<Navbar />
