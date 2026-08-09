6b8207648570fe958c32c5ea4c5844ec9a186e34
Yolisses <iamurise@gmail.com>
2026-08-09 19:42:46 -0300

    Create GlobalHeader

---
+++ Documentation/src/lib/GlobalHeader.svelte
@@ -0,0 +1,9 @@
+<script>
+  import LogoButton from './LogoButton.svelte';
+</script>
+
+<div
+  class="flex flex-row gap-2 md:hidden sticky top-0 dark:bg-zinc-950 bg-gray-200 border-b border-black/10 dark:border-white/10"
+>
+  <LogoButton />
+</div>
--- Documentation/src/lib/GlobalLayout.svelte
+++ Documentation/src/lib/GlobalLayout.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
+  import GlobalHeader from './GlobalHeader.svelte';
   import SidePanel from './SidePanel.svelte';
   import ThemeSetup from './ThemeSetup.svelte';
   const { children } = $props();
 </script>
 
 <ThemeSetup />
-<div class="flex">
-  <SidePanel />
+<div class="flex md:flex-row flex-col">
+  <GlobalHeader />
+  <div class="hidden md:flex">
+    <SidePanel />
+  </div>
   <div class="flex justify-center flex-1 p-2">
-    <main class="justify-center w-full max-w-2xl">
+    <main class="justify-center w-full max-w-2xl md:py-2">
       {@render children()}
     </main>
   </div>
