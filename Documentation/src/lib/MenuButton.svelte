9370d4e9f4dd592d2aa24caca0fc03f8f7412b95
Yolisses <iamurise@gmail.com>
2026-08-09 19:42:46 -0300

    Create GlobalHeader

---
+++ Documentation/src/lib/MenuButton.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { faHamburger } from '@fortawesome/free-solid-svg-icons';
+  import Fa from 'svelte-fa';
+</script>
+
+<button class="basic-button">
+  <Fa icon={faHamburger} />
+</button>
