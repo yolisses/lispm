991bd8ca7b08cccb732e008b429d44479a462c12
Yolisses <iamurise@gmail.com>
2026-08-09 20:01:39 -0300

    Style MainMenu

--- Documentation/src/lib/GlobalHeader.svelte
+++ Documentation/src/lib/GlobalHeader.svelte
@@ -1,9 +1,12 @@
 <script>
   import LogoButton from './LogoButton.svelte';
+  import MenuButton from './MenuButton.svelte';
 </script>
 
 <div
   class="flex flex-row gap-2 md:hidden sticky top-0 dark:bg-zinc-950 bg-gray-200 border-b border-black/10 dark:border-white/10"
 >
   <LogoButton />
+  <div class="flex-1"></div>
+  <MenuButton />
 </div>
