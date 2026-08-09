30b9b49071f244ae8d91664a3b773377ff16967e
Yolisses <iamurise@gmail.com>
2026-08-09 20:01:39 -0300

    Style MainMenu

--- Documentation/src/lib/MenuButton.svelte
+++ Documentation/src/lib/MenuButton.svelte
@@ -1,8 +1,8 @@
 <script>
-  import { faHamburger } from '@fortawesome/free-solid-svg-icons';
+  import { faBars } from '@fortawesome/free-solid-svg-icons';
   import Fa from 'svelte-fa';
 </script>
 
-<button class="basic-button">
-  <Fa icon={faHamburger} />
+<button class="basic-button px-4">
+  <Fa icon={faBars} />
 </button>
