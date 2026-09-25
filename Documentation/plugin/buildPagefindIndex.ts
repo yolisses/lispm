1a5a84f88265cf6abfd855f94c5a5fa15ef0b395
Yolisses <iamurise@gmail.com>
2026-09-25 13:43:06 -0300

    Fix Pagefind output in production build

--- Documentation/plugin/buildPagefindIndex.ts
+++ Documentation/plugin/buildPagefindIndex.ts
@@ -34,6 +34,6 @@ export async function buildPagefindIndex(): Promise<void> {
   }
 
   await index.writeFiles({
-    outputPath: resolve(process.cwd(), 'static/pagefind'),
+    outputPath: resolve(buildDir, 'pagefind'),
   });
 }
