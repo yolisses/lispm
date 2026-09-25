7127f69738999498b9731529e140b5d828968fe3
Yolisses <iamurise@gmail.com>
2026-09-25 16:00:19 -0300

    Fix the search result item html injection problem

--- Documentation/src/lib/search/SearchResultItem.svelte
+++ Documentation/src/lib/search/SearchResultItem.svelte
@@ -2,11 +2,16 @@
   import type { SearchResult } from './SearchResult';
 
   const { result }: { result: SearchResult } = $props();
+  const excerptParts = $derived(result.excerpt.split(/<\/?mark>/g));
 </script>
 
 <a href={result.url} class="bg-highlight not-prose flex flex-col p-2 rounded">
   <div class="font-medium text-xl">
     {result.meta?.title || result.url}
   </div>
-  <div class="text-base">{@html result.excerpt}</div>
+  <div class="text-base">
+    {#each excerptParts as part, index (index)}
+      {#if index % 2 === 1}<mark>{part}</mark>{:else}{part}{/if}
+    {/each}
+  </div>
 </a>
